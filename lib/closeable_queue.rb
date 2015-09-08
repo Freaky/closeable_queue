ed7525ef031c20f3627e4f9a23c9981f1c776b13
Thomas Hurst <tom@hur.st>
2015-09-08 23:07:06 +0100

    Expand tabs.

--- lib/closeable_queue.rb
+++ lib/closeable_queue.rb
@@ -22,108 +22,108 @@ require 'closeable_queue/version'
 # This is anticipated to be obsolete by Ruby 2.3 with Queue#close.
 #
 class CloseableQueue
-	class ClosedQueueError < ThreadError
-	end
+  class ClosedQueueError < ThreadError
+  end
 
-	class ClosedQueue < StopIteration
-	end
+  class ClosedQueue < StopIteration
+  end
 
-	# Set up a new queue.  +limit+ will use a SizedQueue, default unbounded Queue.
-	def initialize(limit = nil)
-		@mutex           = Mutex.new
-		@waiting         = Set.new
-		@num_waiting     = Concurrent::AtomicFixnum.new
-		@closed          = Concurrent::AtomicBoolean.new
-		@raise_exception = Concurrent::AtomicBoolean.new(false)
+  # Set up a new queue.  +limit+ will use a SizedQueue, default unbounded Queue.
+  def initialize(limit = nil)
+    @mutex           = Mutex.new
+    @waiting         = Set.new
+    @num_waiting     = Concurrent::AtomicFixnum.new
+    @closed          = Concurrent::AtomicBoolean.new
+    @raise_exception = Concurrent::AtomicBoolean.new(false)
 
-		if limit
-			@queue = SizedQueue.new(Integer(limit))
-		else
-			@queue = Queue.new
-		end
-	end
+    if limit
+      @queue = SizedQueue.new(Integer(limit))
+    else
+      @queue = Queue.new
+    end
+  end
 
-	def inspect
-		"<#{name} size=#{length} closed=#{@closed.true?}>"
-	end
+  def inspect
+    "<#{name} size=#{length} closed=#{@closed.true?}>"
+  end
 
-	# Take the first element off the queue.
-	#
-	# If the queue is empty and closed?, return nil, or optionally raise
-	# ClosedQueue (a subclass of StopIteration)
-	def pop
-		@queue.pop(true)
-	rescue ThreadError
-		if closed?
-			raise ClosedQueue if @raise_exception.true?
-			return nil
-		else
-			sleep
-			retry
-		end
-	end
+  # Take the first element off the queue.
+  #
+  # If the queue is empty and closed?, return nil, or optionally raise
+  # ClosedQueue (a subclass of StopIteration)
+  def pop
+    @queue.pop(true)
+  rescue ThreadError
+    if closed?
+      raise ClosedQueue if @raise_exception.true?
+      return nil
+    else
+      sleep
+      retry
+    end
+  end
 
-	[:deq, :shift].each { |name| alias_method name, :pop }
+  [:deq, :shift].each { |name| alias_method name, :pop }
 
-	# Add an item to the queue and wakeup any sleeping consumers.
-	#
-	# If the queue is closed, raises +ClosedQueueError+.
-	def push(item)
-		fail ClosedQueueError if closed?
+  # Add an item to the queue and wakeup any sleeping consumers.
+  #
+  # If the queue is closed, raises +ClosedQueueError+.
+  def push(item)
+    fail ClosedQueueError if closed?
 
-		@queue.push(item)
-		wakeup
-		self
-	end
+    @queue.push(item)
+    wakeup
+    self
+  end
 
-	[:enq, :<<].each { |name| alias_method name, :push }
+  [:enq, :<<].each { |name| alias_method name, :push }
 
-	# Get an atomic snapshot if the number of threads waiting on the queue.
-	def num_waiting
-		@num_waiting.value
-	end
+  # Get an atomic snapshot if the number of threads waiting on the queue.
+  def num_waiting
+    @num_waiting.value
+  end
 
-	# Get the number of items remaining on the queue
-	def length
-		@queue.length
-	end
+  # Get the number of items remaining on the queue
+  def length
+    @queue.length
+  end
 
-	# Return true if the queue is empty
-	def empty?
-		@queue.empty?
-	end
+  # Return true if the queue is empty
+  def empty?
+    @queue.empty?
+  end
 
-	# Return true if the queue has been closed
-	def closed?
-		@closed.true?
-	end
+  # Return true if the queue has been closed
+  def closed?
+    @closed.true?
+  end
 
-	# Close the queue if it hasn't been already.  Wake up waiting threads if any.
-	def close(raise_exception = false)
-		@raise_exception.make_true if raise_exception
-		@mutex.synchronize { @waiting.each(&:wakeup) } if @closed.make_true
-		self
-	end
+  # Close the queue if it hasn't been already.  Wake up waiting threads if any.
+  def close(raise_exception = false)
+    @raise_exception.make_true if raise_exception
+    @mutex.synchronize { @waiting.each(&:wakeup) } if @closed.make_true
+    self
+  end
 
-	private
+  private
 
-	def sleep
-		@mutex.synchronize do
-			@waiting << Thread.current
-			@num_waiting.increment
-			@mutex.sleep
-			@waiting.delete(Thread.current)
-			@num_waiting.decrement
-		end
-	end
+  def sleep
+    @mutex.synchronize do
+      @waiting << Thread.current
+      @num_waiting.increment
+      @mutex.sleep
+      @waiting.delete(Thread.current)
+      @num_waiting.decrement
+    end
+  end
 
-	def wakeup
-		return if @num_waiting.value.zero?
+  def wakeup
+    return if @num_waiting.value.zero?
 
-		@mutex.synchronize do
-			@waiting.first.wakeup if @waiting.any?
-		end
-	end
+    @mutex.synchronize do
+      @waiting.first.wakeup if @waiting.any?
+    end
+  end
 end
 
 ClosableQueue = CloseableQueue
--- test/closeable_queue_test.rb
+++ test/closeable_queue_test.rb
@@ -6,25 +6,25 @@ class CloseableQueueTest < Minitest::Test
   end
 
   def closed_queue(items: 0, exception: false)
-  	CloseableQueue.new.tap do |queue|
-  		items.times { queue.push(:test) }
-			queue.close(exception)
-		end
+    CloseableQueue.new.tap do |queue|
+      items.times { queue.push(:test) }
+      queue.close(exception)
+    end
   end
 
   def test_close
-  	queue = closed_queue(items: 1)
+    queue = closed_queue(items: 1)
 
-		assert_equal(queue.pop, :test)
-		assert_nil(queue.pop)
-		assert_raises(CloseableQueue::ClosedQueueError) { queue.push(:test) }
+    assert_equal(queue.pop, :test)
+    assert_nil(queue.pop)
+    assert_raises(CloseableQueue::ClosedQueueError) { queue.push(:test) }
   end
 
-	def test_close_true
-  	queue = closed_queue(items: 1, exception: true)
+  def test_close_true
+    queue = closed_queue(items: 1, exception: true)
 
-		assert_equal(queue.pop, :test)
-		assert_raises(CloseableQueue::ClosedQueueError) { queue.push(:test) }
-		assert_raises(StopIteration) { queue.pop }
-	end
+    assert_equal(queue.pop, :test)
+    assert_raises(CloseableQueue::ClosedQueueError) { queue.push(:test) }
+    assert_raises(StopIteration) { queue.pop }
+  end
 end
